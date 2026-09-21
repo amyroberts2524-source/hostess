fb1c26faaeb92feb354404ef81d87c7ba8283fe1
Lisa Chikovore <lisachikovore@gmail.com>
2026-09-21 23:17:11 +0200

    Serve static files

--- server.js
+++ server.js
@@ -46,6 +46,8 @@ app.use(express.urlencoded({
   extended: true
 }));
 
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'hostessFrontend.html'));
 });
